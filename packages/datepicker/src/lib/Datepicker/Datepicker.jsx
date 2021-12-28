57c16deeb9345b19c534cb62166347e6170a80f9
Abhilash <abhilash.m@card91.io>
2021-12-28 07:48:11 +0530

    calendar undef

--- packages/datepicker/src/lib/Datepicker/Datepicker.jsx
+++ packages/datepicker/src/lib/Datepicker/Datepicker.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "@mui/material/Button";
 
-export function Datepicker() {
+export function CDatepicker() {
   return (
     <div>
       <Button>Calendar btn</Button>
